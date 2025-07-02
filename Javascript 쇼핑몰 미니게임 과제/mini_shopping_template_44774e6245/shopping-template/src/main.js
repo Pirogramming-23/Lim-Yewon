7fe32ddf4afad3b9de6d00600b04a6c804fbc105
Lim-Yewon <yewonim805@gmail.com>
2025-07-02 12:06:57 +0900

    JavaScript-display items

--- Javascript 쇼핑몰 미니게임 과제/mini_shopping_template_44774e6245/shopping-template/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/mini_shopping_template_44774e6245/shopping-template/src/main.js
@@ -2,15 +2,29 @@
 // fetch the items from the JSON file
 function loadItems() {
     return fetch('data/data.json')
-    .then(response => response.json())
-    .then(json => json.items);
+        .then(response => response.json())
+        .then(json => json.items);
+}
+
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+function createHTMLString(item) {
+    return `
+        <li class="item">
+            <img src="${item.image}" alt="${item.type}" class="item_thumnail"/>
+            <span class="item_description">${item.gender}, ${item.size}</span>
+        </li>
+        `;
 }
 
 // main
 loadItems()
-.then(items => {
-    console.log(items)
-//    displayItems(items);
-//    setEventListeners(items)
+    .then(items => {
+        console.log(items)
+        displayItems(items);
+        //    setEventListeners(items)
 })
-.catch(console.log);
+    .catch(console.log);
