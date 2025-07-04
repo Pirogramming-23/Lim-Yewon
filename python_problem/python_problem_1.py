03a5776f51c26d5a9223f68c73f5bcb3da6e2d3d
Lim-Yewon <yewonim805@gmail.com>
2025-07-04 17:22:38 +0900

    6단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,32 +1,26 @@
 num = 0
+players = ["playerA", "playerB"]
+turn = 0
 brNum = 0
 
 while True :
-    try:
-        num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-        num = int(num)
-        if 1 <= num <= 3 :
+    while True :
+        try:
+            num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+            num = int(num)
+            if 1 <= num <= 3 :
+                break
+            else :
+                print("1,2,3 중 하나를 입력하세요")
+        except ValueError :
+            print("정수를 입력하세요")
+
+    for i in range(brNum, brNum+num, 1) :
+        if i > 30 :
             break
-        else :
-            print("1,2,3 중 하나를 입력하세요")
-    except ValueError :
-        print("정수를 입력하세요")
+        print(f"{players[turn%2]}:", i+1)
+    brNum += num
+    turn += 1
 
-for i in range(brNum, brNum+num, 1) :
-    print("playerA:", i+1)
-brNum += num
-
-while True :
-    try:
-        num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-        num = int(num)
-        if 1 <= num <= 3 :
-            break
-        else :
-            print("1,2,3 중 하나를 입력하세요")
-    except ValueError :
-        print("정수를 입력하세요")
-
-for i in range(brNum, brNum+num, 1) :
-    print("playerB:", i+1)
-brNum += num
+    if brNum >= 31 :
+        break
