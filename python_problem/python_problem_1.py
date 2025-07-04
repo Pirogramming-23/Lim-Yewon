5c0271e813029d066fca87dd88484275122d837d
Lim-Yewon <yewonim805@gmail.com>
2025-07-04 16:59:23 +0900

    5단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,4 +1,5 @@
 num = 0
+brNum = 0
 
 while True :
     try:
@@ -11,5 +12,21 @@ while True :
     except ValueError :
         print("정수를 입력하세요")
 
-for i in range(num) :
+for i in range(brNum, brNum+num, 1) :
     print("playerA:", i+1)
+brNum += num
+
+while True :
+    try:
+        num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+        num = int(num)
+        if 1 <= num <= 3 :
+            break
+        else :
+            print("1,2,3 중 하나를 입력하세요")
+    except ValueError :
+        print("정수를 입력하세요")
+
+for i in range(brNum, brNum+num, 1) :
+    print("playerB:", i+1)
+brNum += num
