88cf11c344b3bec4df5f785323e8805333aa2318
Lim-Yewon <yewonim805@gmail.com>
2025-07-04 18:22:05 +0900

    9단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,32 +1,43 @@
+import random
+
 num = 0
-players = ["playerA", "playerB"]
-turn = 0
+players = ["computer", "player"]
+player = 0
 brNum = 0
 
 def brGame() :
-    global num, turn, brNum
-    
-    while True :
-        while True :
-            try:
-                num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-                num = int(num)
-                if 1 <= num <= 3 :
-                    break
-                else :
-                    print("1,2,3 중 하나를 입력하세요")
-            except ValueError :
-                print("정수를 입력하세요")
+    global num, player, brNum
 
+    while True :
+        try:
+            num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+            num = int(num)
+            if 1 <= num <= 3 :
+                break
+            else :
+                print("1,2,3 중 하나를 입력하세요")
+        except ValueError :
+            print("정수를 입력하세요")
+
+    for i in range(brNum, brNum+num, 1) :
+        if i > 30 :
+            break
+        print("player:", i+1)
+
+while True :
+    if player == 0 :
+        num = random.randint(1,3)
         for i in range(brNum, brNum+num, 1) :
             if i > 30 :
                 break
-            print(f"{players[turn%2]}:", i+1)
-        brNum += num
-        turn += 1
+            print("computer:", i+1)
+    elif player == 1 :
+        brGame()
 
-        if brNum >= 31 :
-            break
+    brNum += num
+    player = (player + 1) % 2
 
-brGame()
-print(f"{players[turn%2]} win!")
+    if brNum >= 31 :
+        break
+
+print(f"{players[player%2]} win!")
